b99154776bdf40cb5e85efbbbce86fd7c47da3f0
Coby Coleman <opencloudjedi@gmail.com>
2020-06-17 13:40:13 -0500

    Finished testing and repairing many sudo functions.

--- rogue_one_vagrant.sh
+++ rogue_one_vagrant.sh
@@ -93,7 +93,7 @@ SSH_PORT="2222"
 
 function setup_servera() {
 #Install Apache
-ssh vagrant@server1.eight.example.com "cat > /etc/yum.repos.d/building.repo << EOF
+ssh vagrant@server1.eight.example.com "cat > /home/vagrant/building.repo << EOF
 [BaseOS]
 name=BaseOS
 baseurl=http://repo.eight.example.com/BaseOS
@@ -105,9 +105,10 @@ baseurl=http://repo.eight.example.com/AppStream
 enabled=1
 gpgcheck=0
 EOF
+sudo cp /home/vagrant/building.repo /etc/yum.repos.d/building.repo
 sudo yum install httpd -y &>/dev/null;
 #Create VirtualHost for port 84 with DocumentRoot outside of /var/www/html
-cat > /etc/httpd/conf.d/servera.conf << EOF
+cat > /home/vagrant/servera.conf << EOF
 listen 84
 <VirtualHost *:84>
 	ServerName	localhost
@@ -116,23 +117,26 @@ listen 84
 	ErrorLog	logs/localhost.error.log
 </VirtualHost>
 EOF
+sudo sed -i s/=permissive/=enforcing/g /etc/selinux/config;
+sudo setenforce 1;
+sudo cp /home/vagrant/servera.conf /etc/httpd/conf.d/server1.conf
 sudo mkdir /test
-wget -O /test/index.html http://cloudjedi.org/starwars.html &>/dev/null
+sudo wget -O /test/index.html http://cloudjedi.org/starwars.html &>/dev/null
 #Delete Repositories
-rm -f /etc/yum.repos.d/*.repo;
+sudo rm -f /etc/yum.repos.d/*.repo;
 #Create $FINDUSER
 echo "creating user: ${FINDUSER}";
-useradd $FINDUSER;
+sudo useradd $FINDUSER;
 #Create files to be found $FINDFILES
 echo "creating files for $FINDUSER"
-touch {$FINDFILES};
+sudo touch {$FINDFILES};
 #Change Ownership of those files to the $FINDOWNER
 echo "changing ownership to ${FINDUSER} ";
-chown $FINDUSER:$FINDUSER {$FINDFILES};
+sudo chown $FINDUSER:$FINDUSER {$FINDFILES};
 #Create $GREPFILE
 #wget github.com/OpenCloudJedi/${GREPFILE}
 #Remove firewall rule for Cockpit
-firewall-cmd --zone=public --permanent --remove-service=cockpit;
+sudo firewall-cmd --zone=public --permanent --remove-service=cockpit;
 #Remove networking
 #echo "removing network connection"
 #nmcli con delete "${SERVERACON}";
@@ -148,46 +152,46 @@ function setup_serverb() {
 ssh vagrant@server2.eight.example.com "
 sudo head -c 32 /dev/urandom | passwd --stdin root;
 sudo head -c 32 /dev/urandom | passwd --stdin vagrant;
-sudo echo 'fdisk -u  /dev/vdb <<EOF' >> /root/part;
-sudo echo 'n' >> /root/part;
-sudo echo 'p' >> /root/part;
-sudo echo '1' >> /root/part;
-sudo echo '' >> /root/part;
-sudo echo '+256M' >> /root/part;
-sudo echo 'n' >> /root/part;
-sudo echo 'p' >> /root/part;
-sudo echo '2' >> /root/part;
-sudo echo '' >> /root/part;
-sudo echo '+256M' >> /root/part;
-sudo echo 'n' >> /root/part;
-sudo echo 'p' >> /root/part;
-sudo echo '3' >> /root/part;
-sudo echo '' >> /root/part;
-sudo echo '+256M' >> /root/part;
-sudo echo 'w' >> /root/part;
-sudo echo 'EOF' >> /root/part;
-sudo chmod +x /root/part;
-sudo bash /root/part;
+echo 'fdisk -u  /dev/sdb <<EOF' >> /home/vagrant/part;
+echo 'n' >> /home/vagrant/part;
+echo 'p' >> /home/vagrant/part;
+echo '1' >> /home/vagrant/part;
+echo '' >> /home/vagrant/part;
+echo '+256M' >> /home/vagrant/part;
+echo 'n' >> /home/vagrant/part;
+echo 'p' >> /home/vagrant/part;
+echo '2' >> /home/vagrant/part;
+echo '' >> /home/vagrant/part;
+echo '+256M' >> /home/vagrant/part;
+echo 'n' >> /home/vagrant/part;
+echo 'p' >> /home/vagrant/part;
+echo '3' >> /home/vagrant/part;
+echo '' >> /home/vagrant/part;
+echo '+256M' >> /home/vagrant/part;
+echo 'w' >> /home/vagrant/part;
+echo 'EOF' >> /home/vagrant/part;
+sudo chmod +x /home/vagrant/part;
+sudo bash /home/vagrant/part;
 sudo partprobe;
 #Create existing swap
-sudo mkswap /dev/vdb1 &>/dev/null;
+sudo mkswap /dev/sdb1 &>/dev/null;
 #Create VG and set PE size
-sudo pvcreate /dev/vdb2 /dev/vdb3 &>/dev/null
-sudo vgcreate -s $EXISTINGPESIZE $EXISTINGVGNAME /dev/vdb2 /dev/vdb3 &>/dev/null;
+sudo pvcreate /dev/sdb2 /dev/sdb3 &>/dev/null
+sudo vgcreate -s $EXISTINGPESIZE $EXISTINGVGNAME /dev/sdb2 /dev/sdb3 &>/dev/null;
 #Create LV
 sudo lvcreate -n $EXISTINGLVNAME -L $EXISTINGLVSIZE $EXISTINGVGNAME &>/dev/null;
 #Create FileSystem
 sudo mkfs -t $EXISTINGFSTYPE /dev/${EXISTINGVGNAME}/${EXISTINGLVNAME} &>/dev/null;
 #Add to /etc/fstab
-sudo echo '/dev/$EXISTINGVGNAME/$EXISTINGLVNAME $EXISTINGMOUNTPOINT $EXISTINGFSTYPE defaults 0 0' >> /etc/fstab;
-sudo echo '/dev/vdb1 swap swap defaults 0 0' >> /etc/fstab;
+echo '/dev/$EXISTINGVGNAME/$EXISTINGLVNAME $EXISTINGMOUNTPOINT $EXISTINGFSTYPE defaults 0 0' | sudo tee -a /etc/fstab;
+echo '/dev/sdb1 swap swap defaults 0 0' | sudo tee -a /etc/fstab;
 sudo mkdir ${EXISTINGMOUNTPOINT}
 sudo swapon -a;
 sudo mount -a;
 #Change performance profile from default to anything else...
 sudo tuned-adm profile throughput-performance;
 #Install autofs, but do not enable
-sudo yum install autofs -y &>/dev/null;
+sudo yum remove vim -y &>/dev/null;
 #Extend grub timeout
 #Fix grub
 sudo sed -i s/TIMEOUT=1/TIMEOUT=20/g /etc/default/grub ;
@@ -200,7 +204,7 @@ setup_servera
 setup_serverb
 grep -q empire /etc/hosts
 if [ $? = 1 ]; then
-	sudo echo "172.25.250.10 empire.lab.example.com" >> /etc/hosts;
-	sudo echo "172.25.250.11 rebels.lab.example.com" >> /etc/hosts;
+	echo "172.25.250.10 empire.lab.example.com" | tee -a /etc/hosts;
+	echo "172.25.250.11 rebels.lab.example.com" | tee -a /etc/hosts;
 fi
 echo "The setup script is finished. You may login to servera annd serverb to begin your work."
