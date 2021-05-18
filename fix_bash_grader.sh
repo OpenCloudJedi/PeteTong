9af412b8a583975a836c37efa581bec5a3df145a
Coby Coleman <opencloudjedi@gmail.com>
2021-05-18 16:00:27 -0500

    Update fix_bash_grader.sh

--- fix_bash_grader.sh
+++ fix_bash_grader.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-logger -p authpriv.warn "Failed password for manager1"
+for server in servera serverb; do
+ssh root@${server} "logger -p authpriv.warn "Failed password for manager1"
 logger -p authpriv.warn "Failed password for sysadmin1"
-logger -p authpriv.warn "Failed password for operator1"
+logger -p authpriv.warn "Failed password for operator1""
+done
