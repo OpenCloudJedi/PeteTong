fb914dc806c239fab7d433332dd674e5a1469af1
Coby Coleman <opencloudjedi@gmail.com>
2022-01-31 16:36:28 -0600

    removed SSH

--- find.sh
+++ find.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
   
-ssh root@servera "
-useradd -s /sbin/nologin Bilbo;
-touch /tmp/Precious;
-touch /bin/Ring;
-touch /var/log/Precious2;
-chown Bilbo:Bilbo /tmp/Precious;
-chown Bilbo:Bilbo /bin/Ring;
-chown Bilbo:Bilbo /var/log/Precious2;"
+sudo useradd -s /sbin/nologin Bilbo;
+sudo touch /tmp/Precious;
+sudo touch /bin/Ring;
+sudo touch /var/log/Precious2;
+sudo touch /etc/Cursed
+sudo chown Bilbo:Bilbo /tmp/Precious;
+sudo chown Bilbo:Bilbo /bin/Ring;
+sudo chown Bilbo:Bilbo /var/log/Precious2;
+sudo chown Bilbo:Bilbo /etc/Cursed
